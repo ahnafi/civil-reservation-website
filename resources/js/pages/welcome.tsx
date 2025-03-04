cd6ab66d0f769a4e15ed3d9b2a1f8218a11dbee8
jackberck <berckjack@gmail.com>
2025-03-04 08:36:43 +0700

    feat: adding toggle theme

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -1,5 +1,6 @@
 import { type SharedData } from '@/types';
 import { Head, Link, usePage } from '@inertiajs/react';
+import { ModeToggle } from "@/components/mode-toggle";
 
 export default function Welcome() {
     const { auth } = usePage<SharedData>().props;
