1065c799f9b94cfb40111718ba36c56d9814c429
athallahdx <athallahgamer4@gmail.com>
2025-04-08 21:12:15 +0700

    Modify: New Lab Column

--- database/seeders/PackagesSeeder.php
+++ database/seeders/PackagesSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\Package;
+use App\Models\Laboratory;
 
 class PackagesSeeder extends Seeder
 {
@@ -12,32 +13,39 @@ class PackagesSeeder extends Seeder
      */
     public function run(): void
     {
+        $laboratories = Laboratory::pluck('id', 'code');
+
         $packages = [
             [
                 "name" => "Uji Material Pasir Lengkap",
                 "price" => 600000,
-                "description" => "Paket pemeriksaan ini dilakukan untuk mengetahui berbagai macam sifat pasir."
+                "description" => "Paket pemeriksaan ini dilakukan untuk mengetahui berbagai macam sifat pasir.",
+                "laboratory_id" => $laboratories['LT'], // Lab Tanah
             ],
             [
                 "name" => "Uji Kualitas Air Komprehensif",
                 "price" => 800000,
-                "description" => "Paket pemeriksaan ini dilakukan untuk mengetahui kualitas air secara menyeluruh."
+                "description" => "Paket pemeriksaan ini dilakukan untuk mengetahui kualitas air secara menyeluruh.",
+                "laboratory_id" => $laboratories['LH'], // Lab Hidrologi
             ],
             [
                 "name" => "Uji Kekuatan Beton dan Baja",
                 "price" => 1200000,
-                "description" => "Paket pemeriksaan ini dilakukan untuk mengetahui kekuatan beton dan baja."
+                "description" => "Paket pemeriksaan ini dilakukan untuk mengetahui kekuatan beton dan baja.",
+                "laboratory_id" => $laboratories['LSBB'], // Lab Struktur dan Bahan Bangunan
             ],
             [
                 "name" => "Pemetaan Geologi dan Hidrologi",
                 "price" => 1500000,
-                "description" => "Paket pemeriksaan ini dilakukan untuk mengetahui kondisi geologi dan hidrologi suatu area."
+                "description" => "Paket pemeriksaan ini dilakukan untuk mengetahui kondisi geologi dan hidrologi suatu area.",
+                "laboratory_id" => $laboratories['LP'], // Lab Pemetaan
             ],
             [
                 "name" => "Uji Mekanika Tanah Lengkap",
                 "price" => 1000000,
-                "description" => "Paket pemeriksaan ini dilakukan untuk mengetahui berbagai macam sifat mekanika tanah."
-            ]
+                "description" => "Paket pemeriksaan ini dilakukan untuk mengetahui berbagai macam sifat mekanika tanah.",
+                "laboratory_id" => $laboratories['LMT'], // Lab Mekanika Tanah
+            ],
         ];
 
         foreach ($packages as $package) {
