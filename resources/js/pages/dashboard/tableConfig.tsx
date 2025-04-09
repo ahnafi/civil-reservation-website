b278be90b48973ddb0319fb4f0a75806b596c1d1
athallahdx <athallahgamer4@gmail.com>
2025-04-10 00:13:03 +0700

    Add: Transaction and Testing column definition

--- resources/js/pages/dashboard/tableConfig.tsx
+++ resources/js/pages/dashboard/tableConfig.tsx
@@ -1,10 +1,29 @@
 import { ColumnDef } from "@tanstack/react-table";
-import { type SubmissionSchedule } from "@/types";
+import { type SubmissionSchedule, Transaction, Testing, SimpleOption } from "@/types";
 import * as React from "react";
 import {Button} from "@/components/ui/button";
 import {ArrowUpDown} from "lucide-react";
 
-export const columns: ColumnDef<SubmissionSchedule>[] = [
+// Submission Column Labels
+export const submissionColumnLabels: Record<string, string> = {
+    code: "Kode Pengajuan",
+    test_submission_date: "Tanggal",
+    company_name: "Perusahaan",
+    lab_code: "Lab",
+    test_name: "Jenis Pengujian",
+    status: "Status",
+    detail: "Detail",
+};
+
+// Submission Status Options
+export const submissionStatusOptions: SimpleOption[] = [
+    { id: 1, name: "Approved" },
+    { id: 2, name: "Rejected" },
+    { id: 3, name: "Submitted" },
+];
+
+// Submission Columns Definition
+export const submissionColumns: ColumnDef<SubmissionSchedule>[] = [
     {
         header: "#",
         cell: ({ row }) => row.index + 1,
@@ -87,5 +106,142 @@ export const columns: ColumnDef<SubmissionSchedule>[] = [
                 </div>
             )
         },
+    },
+    {
+        id: "detail",
+        header: () => <div className="flex justify-center text-center">Detail</div>,
+        cell: ({ row }) => (
+            <div className="flex justify-center">
+                <Button
+                    className="cursor-pointer"
+                    onClick={() => handleDetailClick(row.original)}
+                >
+                    Lihat Detail
+                </Button>
+            </div>
+        ),
+    },
+];
+
+// Format Rupiah  Function
+const formatRupiah = (value: number, currency = 'IDR') => {
+    return new Intl.NumberFormat('id-ID', {
+        style: 'currency',
+        currency: currency,
+        minimumFractionDigits: 0,
+        maximumFractionDigits: 0,
+    }).format(value);
+};
+
+// Transaction Column Definition
+export const transactionColumns: ColumnDef<Transaction>[] = [
+    {
+        header: "#",
+        cell: ({ row }) => row.index + 1,
+    },
+    {
+        accessorKey: "code",
+        header: () => <div className="flex justify-center text-center w-[5rem]">Kode Transaksi</div>,
+        cell: ({ row }) => (
+            <div className="capitalize flex justify-center text-center w-[5rem]">{row.getValue("code")}</div>
+        ),
+    },
+    {
+        accessorKey: "amount",
+        header: () => <div className="text-center">Jumlah</div>,
+        cell: ({ row }) => {
+            const amount = row.getValue("amount") as number;
+            return <div className="text-center">{formatRupiah(amount)}</div>;
+        },
+    },
+    {
+        accessorKey: "status",
+        header: () => <div className="text-center">Status Pembayaran</div>,
+        cell: ({ row }) => {
+            const status = row.getValue("status") as string;
+            const statusColor =
+                status === "pending"
+                    ? "bg-yellow-500"
+                    : status === "success"
+                        ? "bg-green-500"
+                        : "bg-red-500";
+
+            return (
+                <div className={`capitalize text-center items-center rounded-2xl py-1  font-medium ${statusColor}`}>
+                    {row.getValue("status")}
+                </div>
+            );
+        }
+    },
+    {
+        id: "detail",
+        header: () => <div className="flex justify-center text-center">Detail</div>,
+        cell: ({ row }) => (
+            <div className="flex justify-center">
+                <Button
+                    className="cursor-pointer"
+                    onClick={() => handleDetailClick(row.original)}
+                >
+                    Lihat Detail
+                </Button>
+            </div>
+        ),
     }
 ];
+
+// Testing Column Definition
+export const testingColumns: ColumnDef<Testing>[] = [
+    {
+        header: "#",
+        cell: ({ row }) => row.index + 1,
+    },
+    {
+        accessorKey: "code",
+        header: () => <div className="flex justify-center text-center w-[5rem]">Kode Pengujian</div>,
+        cell: ({ row }) => (
+            <div className="capitalize flex justify-center text-center w-[5rem]">{row.getValue("code")}</div>
+        ),
+    },
+    {
+         accessorKey: "test_date",
+         header: () => <div className="text-center">Tanggal Pengujian</div>,
+         cell: ({ row }) => {
+             const testDate = row.getValue("test_date") as string;
+             return <div className="text-center">{testDate}</div>;
+         }
+    },
+    {
+        accessorKey: "status",
+        header: () => <div className="text-center">Status Pengujian</div>,
+        cell: ({ row }) => {
+            const status = row.getValue("status") as string;
+            const statusColor =
+                status === "testing"
+                    ? "bg-yellow-500"
+                    : status === "completed"
+                        ? "bg-green-500"
+                        : "bg-red-500";
+
+            return (
+                <div className={`capitalize text-center items-center rounded-2xl py-1  font-medium ${statusColor}`}>
+                    {row.getValue("status")}
+                </div>
+            );
+        }
+    },
+    {
+        id: "detail",
+        header: () => <div className="flex justify-center text-center">Detail</div>,
+        cell: ({ row }) => (
+            <div className="flex justify-center">
+                <Button
+                    className="cursor-pointer"
+                    onClick={() => handleDetailClick(row.original)}
+                >
+                    Lihat Detail
+                </Button>
+            </div>
+        ),
+    }
+];
+
