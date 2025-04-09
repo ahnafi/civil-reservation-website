49c56335b680d6e891dc88ababb8aec20e0cdd06
athallahdx <athallahgamer4@gmail.com>
2025-04-10 00:08:58 +0700

    Modify: Add Necessary Select for props data passing

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 use App\Models\Laboratory;
 use App\Models\Package;
-use App\Models\submission;
+use App\Models\Submission;
+use App\Models\Testing;
+use App\Models\Transaction;
 use App\Models\Test;
 use Inertia\Inertia;
 use Inertia\Response;
@@ -12,13 +14,31 @@ class DashboardController extends Controller
 {
     public function index(): Response
     {
-        $userSubmissions = submission::withUserScheduleJoin()->get();
+        $userSubmissions = Submission::withUserScheduleJoin()->get();
+
+        $userTransactions = Transaction::query()
+            ->join('submissions', 'transactions.submission_id', '=', 'submissions.id')
+            ->where('submissions.user_id', auth()->id())
+            ->orderBy('transactions.created_at', 'desc')
+            ->select('transactions.*')
+            ->get();
+
+
+        $userTestings = Testing::query()
+            ->join('submissions', 'testings.submission_id', '=', 'submissions.id')
+            ->where('submissions.user_id', auth()->id())
+            ->orderBy('testings.created_at', 'desc')
+            ->select('testings.*')
+            ->get();
+
         $tests = Test::select(['id', 'name'])->get();
         $packages = Package::select(['id', 'name'])->get();
-        $laboratories = laboratory::select(['id', 'code', 'name'])->get();
+        $laboratories = Laboratory::select(['id', 'code', 'name'])->get();
 
         return Inertia::render('dashboard/index', [
             'userSubmissions' => $userSubmissions,
+            'userTransactions' => $userTransactions,
+            'userTestings' => $userTestings,
             'tests' => $tests,
             'packages' => $packages,
             'laboratories' => $laboratories,
