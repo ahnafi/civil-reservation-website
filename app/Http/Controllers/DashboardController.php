f7e631d19c0b63ef8c872af6da36ad52c8ebafd7
athallahdx <athallahgamer4@gmail.com>
2025-04-09 17:14:01 +0700

    Add: Controller for dashboard page

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -1,8 +1,27 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Laboratory;
+use App\Models\Package;
+use App\Models\submission;
+use App\Models\Test;
+use Inertia\Inertia;
+use Inertia\Response;
 
-class DashboardController
+class DashboardController extends Controller
 {
+    public function index(): Response
+    {
+        $userSubmissions = submission::withUserScheduleJoin()->get();
+        $tests = Test::select(['id', 'name'])->get();
+        $packages = Package::select(['id', 'name'])->get();
+        $laboratories = laboratory::select(['id', 'code', 'name'])->get();
 
+        return Inertia::render('dashboard/index', [
+            'userSubmissions' => $userSubmissions,
+            'tests' => $tests,
+            'packages' => $packages,
+            'laboratories' => $laboratories,
+        ]);
+    }
 }
