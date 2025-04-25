414094cab02f8d8e086f04da8a038ec39b2157b9
athallahdx <athallahgamer4@gmail.com>
2025-04-25 07:57:00 +0700

    Modify: Refactor Test Controller

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -5,18 +5,20 @@ namespace App\Http\Controllers;
 use App\Models\Test;
 use Inertia\Inertia;
 use Inertia\Response;
+use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
 
 class TestController extends Controller
 {
     public function index(): Response
     {
-        $tests = Test::active()->with(["category", "laboratory"])->paginate(16);
+        $tests = Test::active()->with(["category", "laboratory"])->paginate(40);
         return Inertia::render("tests/index", ['tests' => $tests]);
     }
 
-    public function detail(Test $test): \Illuminate\Http\RedirectResponse|Response
+    public function detail(Test $test): RedirectResponse|Response
     {
         if (!$test->is_active) return redirect()->route('tests');
         return Inertia::render("tests/detail", ['test' => $test->load("category", "laboratory")]);
     }
-}
+    }
