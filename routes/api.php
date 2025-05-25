215052d0b069ae0e5ae5c10bf38625a8c9d969cf
ahnafi <devsulthon@gmail.com>
2025-05-25 15:49:25 +0700

    feat : create news controller api

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\DownloadController;
+use App\Http\Controllers\NewsController;
 use App\Http\Controllers\ResearchController;
 use App\Http\Controllers\TeamController;
 use Illuminate\Support\Facades\Route;
@@ -14,5 +15,9 @@ Route::get("research", [ResearchController::class, "index"])->name("research.ind
 Route::get("research/search", [ResearchController::class, "search"])->name("research.search");
 
 // Download Routes
-Route::get("downloads",[DownloadController::class,"index"])->name("download.index");
+Route::get("downloads", [DownloadController::class, "index"])->name("download.index");
 Route::get("downloads/search", [DownloadController::class, "search"])->name("download.search");
+
+Route::get("news", [NewsController::class, "index"])->name("news.index");
+Route::get("news/{news:slug}", [NewsController::class, "show"])->name("news.show");
+Route::get("news/search", [NewsController::class, "search"])->name("news.search");
