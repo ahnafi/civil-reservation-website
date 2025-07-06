9745352cd3c44cd799427584074ecb75d3116baa
athallahdx <athallahgamer4@gmail.com>
2025-07-06 19:37:43 +0700

    refactor(app sidebar): remove 'Hasil Pengujian' item and add 'Panduan Reservasi' to enhance navigation

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -61,11 +61,6 @@ const orderNavItems: NavItem[] = [
         url: '/orders/cart',
         icon: ClipboardPen,
     },
-    {
-        title: 'Hasil Pengujian',
-        url: '/',
-        icon: FileText,
-    },
 ];
 
 const historyNavItems: NavItem[] = [
@@ -84,6 +79,14 @@ const historyNavItems: NavItem[] = [
         url: '/history/tests',
         icon: History,
     },
+];
+
+const otherNavItems: NavItem[] = [
+    {
+        title: 'Panduan Reservasi',
+        url: '/tutorial',
+        icon: BookOpen,
+    }
 ];
 
 const adminItems: NavItem[] = [
@@ -122,6 +125,7 @@ export function AppSidebar() {
                 <NavMain label="Layanan" items={serviceNavItems} />
                 <NavMain label="Reservasi" items={orderNavItems} />
                 <NavMain label="Riwayat" items={historyNavItems} />
+                <NavMain label="Lainnya" items={otherNavItems} />
                 { user.role === 'admin' && (
                     <NavMain label="Admin" items={adminItems} />
                 )}
